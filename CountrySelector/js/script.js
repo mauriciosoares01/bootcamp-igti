9393d3c426c9cd6f97274cf74b096c41e9f6c820
mauriciosoares01 <mss.soares@hotmail.com>
2020-05-27 22:25:37 -0300

    list countries and add to favorites

--- CountrySelector/js/script.js
+++ CountrySelector/js/script.js
@@ -6,8 +6,8 @@ let tabCountries = null;
 let allCountries = [];
 let allFavorites = [];
 
-let countCountries = 0;
-let countFavorites = 0;
+let countCountries;
+let countFavorites;
 
 let totalPopulationCountries = 0;
 let totalPopulationFavorites = 0;
@@ -43,5 +43,99 @@ async function getCountries() {
       flag,
     };
   });
-  console.log(allCountries);
+
+  render();
 }
+
+function render() {
+  renderCountriesList();
+  renderFavorites();
+  renderSummary();
+
+  handleCountryButtons();
+}
+
+function renderCountriesList() {
+  let countriesHTML = "<div>";
+  allCountries.forEach((country) => {
+    const { name, flag, id, population } = country;
+    const countryHTML = `
+      <div class='country'>
+        <div>
+          <a id='${id}' class="waves-effect waves-light btn" >+</a>
+        </div>
+        <img id='flagImage' src="${flag}" alt></img>
+        <div id='countryInfo'>
+          <ul>
+            ${name}
+          </ul>
+          <ul>
+            ${population}
+          </ul>
+        </div>
+      </div>
+    `;
+    countriesHTML += countryHTML;
+  });
+  countriesHTML += "</div>";
+  tabCountries.innerHTML = countriesHTML;
+}
+
+function renderFavorites() {
+  let favoritesHTML = "<div>";
+  allFavorites.forEach((country) => {
+    const { name, flag, id, population } = country;
+    const favoriteCountryHTML = `
+      <div class='country'>
+        <div>
+          <a id='${id}' class="waves-effect waves-light btn red" >-</a>
+        </div>
+        <img id='flagImage' src="${flag}" alt></img>
+        <div id='countryInfo'>
+          <ul>${name}</ul>
+          <ul>${population}</ul>
+        </div>
+      </div>
+    `;
+    favoritesHTML += favoriteCountryHTML;
+  });
+  favoritesHTML += "</div/";
+  tabFavorites.innerHTML += favoritesHTML;
+}
+function renderSummary() {
+  countCountries.textContent = `Countries (${allCountries.length})`;
+  countFavorites.textContent = `Favorites countries (${allFavorites.length})`;
+  const reducer = (acc, curr) => acc + curr.population;
+  totalPopulationCountries.textContent = `Total population: ${allCountries.reduce(
+    reducer,
+    0
+  )}`;
+  totalPopulationFavorites.textContent = `Total population: ${allFavorites.reduce(
+    reducer,
+    0
+  )}`;
+}
+function handleCountryButtons() {
+  const countryButtons = Array.from(tabCountries.querySelectorAll(".btn"));
+  const favoritesButtons = Array.from(tabFavorites.querySelectorAll(".btn"));
+
+  countryButtons.forEach((button) => {
+    button.addEventListener("click", () => addToFavorites(button.id));
+  });
+  favoritesButtons.forEach((button) => {
+    button.addEventListener("click", () => addToFavorites(button.id));
+  });
+}
+
+function addToFavorites(id) {
+  const countryToAdd = allCountries.find((country) => country.id === id);
+  allFavorites = allFavorites.concat(countryToAdd);
+  allFavorites.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+
+  allCountries = allCountries.filter((country) => country.id !== id);
+  render();
+}
+
+function removeFromFavorites(id) {}
