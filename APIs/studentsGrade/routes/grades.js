fd3798fd1b12f70cc50a44fd97295f121d68da23
mauriciosoares01 <mss.soares@hotmail.com>
2020-06-09 22:20:23 -0300

    create and update route

--- APIs/studentsGrade/routes/grades.js
+++ APIs/studentsGrade/routes/grades.js
@@ -1,4 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const { promisify } = require("util");
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+global.file = "./grades.json";
+
+router.post("/", async (req, res) => {
+  try {
+    let grade = req.body;
+    const data = await readFile(global.file, "utf8");
+    const json = JSON.parse(data);
+    grade = { id: json.nextId++, ...grade, timestamp: new Date() };
+    json.grades.push(grade);
+    await writeFile(global.file, JSON.stringify(json));
+    res.send({ ok: true, message: "Grade salva com sucesso" });
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const grade = req.body;
+    const id = req.params.id;
+    let data = JSON.parse(await readFile(global.file, "utf8"));
+    const index = data.grades.findIndex((item) => item.id == id);
+    if (index !== -1) {
+      data.grades[index] = { ...data.grades[index], ...grade };
+      await writeFile(global.file, JSON.stringify(data));
+      res.send({ ok: true, message: "Registro atualizado com sucesso" });
+    } else {
+      res
+        .sendStatus(404)
+        .send({ ok: false, message: "Registro não encontrado" });
+    }
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 module.exports = router;
