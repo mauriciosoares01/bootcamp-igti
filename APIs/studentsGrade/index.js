bd6d2ed9d3fc2816b86ce6cdc898ecfb786e2ece
mauriciosoares01 <mss.soares@hotmail.com>
2020-06-10 22:07:16 -0300

    start last route

--- APIs/studentsGrade/index.js
+++ APIs/studentsGrade/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const router = express.Router();
 const port = 3000;
 const app = express();
 const gradesRouter = require("./routes/grades.js");
+const fs = require("fs");
+const { promisify } = require("util");
+
+const readFile = promisify(fs.readFile);
 
 app.use(express.json());
 app.use("/grades", gradesRouter);
@@ -10,6 +13,55 @@ app.use("/grades", gradesRouter);
 app.listen(port, () => {
   console.log("API started");
 });
+
+app.get("/student", async (req, res) => {
+  try {
+    const body = req.body;
+    const data = JSON.parse(await readFile(global.file, "utf8"));
+    const studentGrades = data.grades.filter(
+      (item) => item.student === body.student && item.subject === body.subject
+    );
+    res.send({
+      ok: true,
+      data: studentGrades.reduce((acc, curr) => acc + curr.value, 0),
+    });
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+app.get("/average", async (req, res) => {
+  try {
+    const body = req.body;
+    const data = JSON.parse(await readFile(global.file, "utf8"));
+    const average = await getAverage(body, data);
+    res.send({ ok: true, data: average });
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+app.get("/rank", async (req, res) => {
+  try {
+    const body = req.body;
+    const data = JSON.parse(await readFile(global.file, "utf8"));
+    const average = await getAverage(body, data);
+    res.send({ ok: true, data: average });
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+async function getAverage(body, data) {
+  try {
+    const grades = data.grades.filter(
+      (item) => item.subject === body.subject && item.type === body.type
+    );
+    return grades.reduce((acc, curr) => acc + curr.value, 0) / grades.length;
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+}
 
 app.get("/", (_req, res) => {
   try {
--- APIs/studentsGrade/routes/grades.js
+++ APIs/studentsGrade/routes/grades.js
@@ -44,7 +44,38 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-  } catch (error) {}
+    const id = req.params.id;
+    let data = JSON.parse(await readFile(global.file, "utf8"));
+    const index = data.grades.findIndex((item) => item.id == id);
+    if (index !== -1) {
+      data.grades.splice(index, 1);
+      await writeFile(global.file, JSON.stringify(data));
+      res.send({ ok: true, message: "Registro removido com sucesso" });
+    } else {
+      res
+        .sendStatus(404)
+        .send({ ok: false, message: "Registro não encontrado" });
+    }
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    let data = JSON.parse(await readFile(global.file, "utf8"));
+    const index = data.grades.findIndex((item) => item.id == id);
+    if (index !== -1) {
+      res.send({ ok: true, data: data.grades[index] });
+    } else {
+      res
+        .sendStatus(404)
+        .send({ ok: false, message: "Registro não encontrado" });
+    }
+  } catch (error) {
+    res.sendStatus(400).send({ ok: false, message: error.message });
+  }
 });
 
 module.exports = router;
